4ffd5b85e4f8d4256741615b2230b2af3f05b47e
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-07-14 23:50:25 +0300

    2.2 - 2

---
+++ 2. Проверка готовности:)/2.2 Интересности/2.py
@@ -0,0 +1,7 @@
+count = int(input())
+
+kinder_price = 60
+
+print("Цена одного киндера:", kinder_price)
+print("Сколько вы хотите купить:", count)
+print("Общая стоимость покупки:", kinder_price * count)
