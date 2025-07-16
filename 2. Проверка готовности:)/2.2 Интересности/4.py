e548bcc42a1ac0a13466b110b667bd8b98227a59
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-07-16 23:17:34 +0300

    2.2 - 4

---
+++ 2. Проверка готовности:)/2.2 Интересности/4.py
@@ -0,0 +1,9 @@
+money, count = int(input()), int(input())
+
+cola_price = 80
+y = cola_price * count
+z = money - y
+
+print("Стоимость 1 бутылки:", cola_price)
+print("Стоимость всей покупки:", y)
+print("Сколько денег останется после покупки:", z)
