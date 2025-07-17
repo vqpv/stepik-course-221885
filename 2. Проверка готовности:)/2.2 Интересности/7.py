f5dc857d2f1205928bbf2b14b62c6b3f41198cc4
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-07-17 21:42:45 +0300

    2.2 - 7

---
+++ 2. Проверка готовности:)/2.2 Интересности/7.py
@@ -0,0 +1,7 @@
+attack_power = int(input())
+intelligence = int(input())
+disobedience = int(input())
+
+result = attack_power * 4 + intelligence * 5 - disobedience * 2
+
+print("Боеспособность персонажа:", result)
