9f088b6c1c43282b2fea136078102a15589d6915
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-07-28 23:11:10 +0300

    4.2 - 4

---
+++ 4. Разбираемся с and и or./4.2 Товарищ or/4.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = input()
+
+if a >= b or c == "да":
+    print("Программист получает новую BMW M5")
+else:
+    print("Программист продолжает ездить на прошлогодней Audi")
