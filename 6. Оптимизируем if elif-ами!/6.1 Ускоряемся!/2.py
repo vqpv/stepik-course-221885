b03106f8ba1be00df53a152ec9c8a1e1008dea09
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-07-30 23:52:41 +0300

    6.1 - 2

---
+++ 6. Оптимизируем if elif-ами!/6.1 Ускоряемся!/2.py
@@ -0,0 +1,12 @@
+dish = input()
+
+s = "Вы получаете"
+
+if dish == "пицца":
+    print(s, "сок")
+elif dish == "суп":
+    print(s, "гренки")
+elif dish == "роллы":
+    print(s, "соус")
+elif dish == "печенька":
+    print(s, "мороженое")
