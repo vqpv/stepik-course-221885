4c92b14b238ec8e87cf9598be07966bf1ffab1b8
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-07-31 22:24:26 +0300

    6.2 - 1

---
+++ 6. Оптимизируем if elif-ами!/6.2 Ещё быстрее!/1.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+if n == 1:
+    print("Niletto - Любимка")
+elif n == 2:
+    print("Баста - Медлячок")
+elif n == 3:
+    print("Instasamka - За деньги да")
+elif n == 4:
+    print("Anna Asti - Царица")
+elif n == 5:
+    print("Шаман - Я Русский!")
