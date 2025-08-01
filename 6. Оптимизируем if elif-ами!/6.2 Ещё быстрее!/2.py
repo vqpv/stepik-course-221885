463b020a6967e9cd3e5cd209b80ee1997fad62c6
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-08-01 23:45:00 +0300

    6.2 - 2

---
+++ 6. Оптимизируем if elif-ами!/6.2 Ещё быстрее!/2.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = input()
+c = int(input())
+
+if b == "+":
+    print(a + c)
+elif b == "-":
+    print(a - c)
+elif b == "*":
+    print(a * c)
+elif b == "/":
+    print(a / c)
