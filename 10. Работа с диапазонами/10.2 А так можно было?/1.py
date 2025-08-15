5bf79a6c9b8748026a52bea49ae23a2259eaa9b4
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-08-15 21:22:40 +0300

    10.2 - 1

---
+++ 10. Работа с диапазонами/10.2 А так можно было?/1.py
@@ -0,0 +1,14 @@
+w = int(input())
+
+a = 35
+b = 50
+c = 65
+
+if w < a:
+    print("Группа 1")
+elif w < b:
+    print("Группа 2")
+elif w < c:
+    print("Группа 3")
+elif w > c:
+    print("Группа 4")
