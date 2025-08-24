beb4ad3609ace743ce5d21ba529d90e2e674da04
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-08-24 17:10:46 +0300

    15.1 - 4

---
+++ 15. Рандом! Игровые механики/15.1 Я случайно.../4.py
@@ -0,0 +1,13 @@
+import random
+
+
+random_number = random.randint(1, 4)
+
+if random_number == 1:
+    print("Заморозка времени")
+elif random_number == 2:
+    print("Невидимость")
+elif random_number == 3:
+    print("Чтение мыслей")
+elif random_number == 4:
+    print("Защитное поле")
