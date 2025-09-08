8dd37ac05d503f6c3e0d6734911a85b10f3d9129
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-09-08 23:42:50 +0300

    19.1 - 4

---
+++ 19. Циклы. Больше взаимодействия/19.1 Цикл for plus/4.py
@@ -0,0 +1,8 @@
+for _ in range(2):
+    x = input()
+    y = input()
+    z = input()
+    
+    print("Уважаемый", x)
+    print("Вы заказали в столовой", y)
+    print("И я записал вас на кружок", z)
