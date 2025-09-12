cf24ac6a50e57783094248c814272c506c3abfce
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-09-12 22:38:25 +0300

    21.1 - 4

---
+++ 21. Финальный проект!/21.1 Первая игра на питоне!/4.py
@@ -0,0 +1,22 @@
+import random
+
+
+number_guesses = 0
+
+for _ in range(3):
+    x = int(input())
+    
+    n = random.randint(1, 3)
+    
+    if x == n:
+        number_guesses += 1
+        print("Вау, вы настоящий волшебник!")
+    else:
+        print("Увы, в этот раз не повезло!")
+
+print("Количество отгадок:", number_guesses)
+
+if number_guesses >= 2:
+    print("Вау, вы сегодня были в ударе!")
+else:
+    print("Неплохой результат, но можно лучше!")
